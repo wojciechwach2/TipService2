78a00fe3e7d6ef470591cbc03742368705323db8
wojciechwach2 <wojciech.wach2@gmail.com>
2023-01-14 16:15:55 +0100

    html

--- src/main/java/com/example/TipService/TipServiceApplication.java
+++ src/main/java/com/example/TipService/TipServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.porada;
+package com.example.TipService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/TipService/config/WebSecurityConfig.java
+++ src/main/java/com/example/TipService/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.porada.config;
+package com.example.TipService.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
