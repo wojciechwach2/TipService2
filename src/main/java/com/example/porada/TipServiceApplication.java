1946abf00dd1f9872f8c80b862e5f8fb811e4be1
wojciechwach2 <wojciech.wach2@gmail.com>
2023-01-14 15:45:18 +0100

    security

---
+++ src/main/java/com/example/porada/TipServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.porada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TipServiceApplication.class, args);
+    }
+
+}
