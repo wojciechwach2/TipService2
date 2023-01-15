882cb328bb9dac042d176a131d13e20616e576e2
wojciechwach2 <wojciech.wach2@gmail.com>
2023-01-15 10:52:20 +0100

    template

---
+++ src/main/java/com/example/TipService/controllers/MainPageController.java
@@ -0,0 +1,15 @@
+package com.example.TipService.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+
+
+    @GetMapping("/main")
+    public String getMainPage( String index){
+
+       return "index";
+    }
+}
