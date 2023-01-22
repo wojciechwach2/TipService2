dadef8f8567ca4fb09f380ba5733355215db55ba
wojciechwach2 <wojciech.wach2@gmail.com>
2023-01-22 16:01:25 +0100

    #Templates + CSS + js #2

--- src/main/java/com/example/TipService/model/CategoryDto.java
+++ src/main/java/com/example/TipService/model/CategoryDto.java
@@ -7,8 +7,7 @@ import org.springframework.stereotype.Component;
 @Setter
 @Component
 public class CategoryDto {
-    // klasę z odpowiednimi polami i setterami/getterami. Klasa ta powinna zawierać tylko te pola, które są potrzebne
-    // dla danej warstwy aplikacji. Następnie należy użyć tej klasy w kontrolerze lub serwisie, aby przesyłać dane między warstwami.
+
 
     private Long categoryId;
     private String categoryName;
--- src/main/java/com/example/TipService/services/CategoryService.java
+++ src/main/java/com/example/TipService/services/CategoryService.java
@@ -1,9 +1,11 @@
 package com.example.TipService.services;
 
 import com.example.TipService.dao.CategoryRepository;
+import com.example.TipService.entities.CategoryEntity;
 import com.example.TipService.model.CategoryDto;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,7 +18,22 @@ public class CategoryService {
 
 
     }
-    public List<CategoryDto> getAllCategories(){
+
+    public List<CategoryDto> getAllCategories() {
+        List<CategoryEntity> categories = categoryRepository.findAll();
+
+
         return null;
+    }
+
+    private List<CategoryDto> convertEntityToDto(List<CategoryEntity> entities) {
+        List<CategoryDto> result = new ArrayList<>();
+        for (CategoryEntity entity : entities) {
+            CategoryDto categoryDto = new CategoryDto();
+            categoryDto.setCategoryId(entity.getId());
+            categoryDto.setCategoryName(entity.getName());
+            result.add(categoryDto);
+        }
+        return result;
     }
 }
