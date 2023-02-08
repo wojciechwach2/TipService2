c185d43009ed12635ce71de1eea0a5e6bec2943d
wojciechwach2 <wojciech.wach2@gmail.com>
2023-02-08 13:15:37 +0100

    #Update 8 - testy

---
+++ src/test/java/com/example/TipService/dao/UserRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.example.TipService.dao;
+
+import com.example.TipService.entities.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateUser() {
+        UserEntity user = new UserEntity();
+        user.setEmail("kubaRozpruwacz@gmail.com");
+        user.setPassword("Ragnarok");
+
+
+        UserEntity savedUser = repo.save(user);
+
+        UserEntity existUser = entityManager.find(UserEntity.class, savedUser.getId());
+
+        assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+
+    }
+}
